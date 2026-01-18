592e3c35e985ad0d5d3ad023e1205fcb0f2d243c
blake-transcend <blake@transcend.io>
2026-01-17 22:44:07 -0800

    db fixes

--- backend/db/database.ts
+++ backend/db/database.ts
@@ -4,12 +4,13 @@ import { User } from './models/user.model.js';
 import { AuthenticationMethod } from './models/authentication-method.model.js';
 import { EmailAccount } from './models/email-account.model.js';
 import { SmtpProfile } from './models/smtp-profile.model.js';
-import { Email } from './models/email.model.js';
 import { ContactEmail } from './models/contact-email.model.js';
 import { Contact } from './models/contact.model.js';
 import { Tag } from './models/tag.model.js';
 import { EmailTag } from './models/email-tag.model.js';
 import { MailRule } from './models/mail-rule.model.js';
+// Import Email before Attachment to avoid circular dependency
+import { Email } from './models/email.model.js';
 import { Attachment } from './models/attachment.model.js';
 
 // Validate environment in production
